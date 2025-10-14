ecdf142f4ecc49b0da1405733c353de55c76a04c
Songzex <2402265378@qq.com>
2025-10-14 14:08:17 +0800

    first1.1.1

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="layout-container">
+    <Sidebar></Sidebar>
     <router-view />
   </div>
 </template>
 
 <script setup>
+import Sidebar from '/src/components/silder/index.vue'
 // 布局组件，用于包裹页面内容
+// import Sidebar from './components/silder/index.vue'
 </script>
 
 <style scoped>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div>仪表盘</div>
+</template>
+
+<style scoped>
+
+</style>
